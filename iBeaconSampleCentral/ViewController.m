5361efa63d11e5de82e6889f8b53face593c38b9
genshun9 <genshun@fringe81.com>
2016-03-05 12:17:39 +0900

    first commit

---
+++ iBeaconSampleCentral/ViewController.m
@@ -0,0 +1,326 @@
+//
+//  ViewController.m
+//  iBeaconSampleCentral
+//
+//  Created by iScene on 2015/06/07.
+//  Copyright (c) 2015年 iScene. All rights reserved.
+//
+//  iOS8からはロケーションマネージャの権限取得
+//  http://it.senatus.jp/post-210/
+//
+//  [iOS 7] 新たな領域観測サービス iBeacon を使ってみる
+//  http://dev.classmethod.jp/references/ios7-ibeacon-api/
+//
+//  セントラル受信側
+//  情報を受取るセントラル(デバイス)
+
+#import "ViewController.h"
+#import <CoreLocation/CoreLocation.h>
+
+#define UUID        @"C3373A41-8E97-4145-BC2B-B2DE7587C52F"
+#define IDENTIFIER  @"genshun"
+
+@interface ViewController () <CLLocationManagerDelegate>
+
+@property (nonatomic) CLLocationManager *locationManager;
+@property (nonatomic) CLBeaconRegion *beaconRegion;
+
+@property (nonatomic) NSUUID                        *proximityUUID;
+@property (strong, nonatomic) NSString              *identifier;
+@property uint16_t                                  major;
+@property uint16_t                                  minor;
+
+@property (weak, nonatomic) IBOutlet UILabel *beaconFoundLabel;
+@property (weak, nonatomic) IBOutlet UILabel *proximityUUIDLabel;
+@property (weak, nonatomic) IBOutlet UILabel *majorLabel;
+@property (weak, nonatomic) IBOutlet UILabel *minorLabel;
+@property (weak, nonatomic) IBOutlet UILabel *accuracyLabel;
+@property (weak, nonatomic) IBOutlet UILabel *distanceLabel;
+@property (weak, nonatomic) IBOutlet UILabel *rssiLabel;
+
+@property (weak, nonatomic) IBOutlet UITextView *tvdisply;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    NSLog(@"UNKO1");
+    [super viewDidLoad];
+    
+    //self.tvdisply.text = nil;
+    
+    if ([CLLocationManager isMonitoringAvailableForClass:[CLCircularRegion class]]) {
+        
+        // CLLocationManagerの生成とデリゲートの設定
+        self.locationManager = [CLLocationManager new];
+        self.locationManager.delegate = self;
+        
+        // 生成したUUIDからNSUUIDを作成
+        self.proximityUUID      = [[NSUUID alloc]initWithUUIDString:UUID];
+        self.identifier         = IDENTIFIER;
+        
+        self.beaconRegion = [[CLBeaconRegion alloc] initWithProximityUUID:self.proximityUUID
+                                                                    major:self.major
+                                                                    minor:self.minor
+                                                               identifier:self.identifier];
+        
+        self.beaconRegion.notifyOnEntry               = YES; // 領域に入った事を監視 YES
+        self.beaconRegion.notifyOnExit                = NO; // 領域を出た事を監視
+        self.beaconRegion.notifyEntryStateOnDisplay   = YES; // デバイスのディスプレイがオンのとき、ビーコン通知が送信されない NO
+        
+        /////////////////////////////////
+        // iOS8の追加
+        // 位置情報の取得許可を求めるメソッド
+        if ([self.locationManager respondsToSelector:@selector(requestAlwaysAuthorization)]) {
+            // requestAlwaysAuthorizationメソッドが利用できる場合(iOS8以上の場合)
+            // 位置情報の取得許可を求めるメソッド
+            [self.locationManager requestAlwaysAuthorization];
+        } else {
+            // requestAlwaysAuthorizationメソッドが利用できない場合(iOS8未満の場合)
+            [self.locationManager startMonitoringForRegion: self.beaconRegion];
+        }
+        /////////////////////////////////
+        
+        // Beaconによる領域観測を開始
+        //[self.locationManager startMonitoringForRegion:self.beaconRegion];
+    }
+}
+
+- (void)didReceiveMemoryWarning
+{
+    NSLog(@"UNKO2");
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - CLLocationManagerDelegate methods
+
+- (void)locationManager:(CLLocationManager *)manager didStartMonitoringForRegion:(CLRegion *)region
+{
+    NSLog(@"UNKO3");
+    [self sendLocalNotificationForMessage:@"地域の監視を開始 Start Monitoring Region"];
+}
+
+//-------------------------------------
+// 領域に入った時
+// Beacon内の領域に入る（領域観測）Region（レンジング）　ローカル通知を送っている
+- (void)locationManager:(CLLocationManager *)manager didEnterRegion:(CLRegion *)region
+{
+    // ローカル通知
+    NSLog(@"UNKO4");
+    [self sendLocalNotificationForMessage:@"地域入力 Enter Region"];
+    // Beaconの距離測定を開始する
+    if ([region isMemberOfClass:[CLBeaconRegion class]] && [CLLocationManager isRangingAvailable]) {
+        [self.locationManager startRangingBeaconsInRegion:(CLBeaconRegion *)region];
+    }
+}
+
+//
+- (void)locationManager:(CLLocationManager *)manager didExitRegion:(CLRegion *)region
+{
+    NSLog(@"UNKO5");
+    // ローカル通知
+    [self sendLocalNotificationForMessage:@"出口エリア Exit Region"];
+    // Beaconの距離測定を終了する
+    if ([region isMemberOfClass:[CLBeaconRegion class]] && [CLLocationManager isRangingAvailable]) {
+        [self.locationManager stopRangingBeaconsInRegion:(CLBeaconRegion *)region];
+    }
+}
+
+// メソッドの２番目の引数には、距離測定中の Beacon の配列が渡されてきます。この配列は、Beacon までの距離が近い順にソートされていますので、先頭に格納されている CLBeacon のインスタンスが最も距離が近い Beacon の情報となります
+// Beacon距離観測 定期的イベント発生（距離の測定を開始）
+- (void)locationManager:(CLLocationManager *)manager didRangeBeacons:(NSArray *)beacons inRegion:(CLBeaconRegion *)region
+{
+ 
+//    NSLog(@"UNKO6");
+    if (beacons.count > 0) {
+        // 最も距離の近いBeaconについて処理する
+        
+        CLBeacon *nearestBeacon = beacons.firstObject;
+        NSString *rangeMessage;
+        
+        // Beacon の距離でメッセージを変える
+        switch (nearestBeacon.proximity) {
+            case CLProximityImmediate:
+                rangeMessage = @"より近い\n ";
+                self.distanceLabel.text = @"より近い";
+                {
+                    // 送信したいURLを作成し、Requestを作成します。
+                    NSURL *url = [NSURL URLWithString:@"/sa?uu=C3373A41-8E97-4145-BC2B-B2DE7587C52F&mj=$0&mn=0&px=$1&id=$1"];
+                    NSURLRequest  *request = [[NSURLRequest alloc] initWithURL:url];
+                
+                    // NSURLConnectionのインスタンスを作成したら、すぐに指定したURLへリクエストを送信し始めます。
+                    // delegate指定すると、サーバーからデータを受信したり、エラーが発生したりするとメソッドが呼び出される。
+                    NSURLConnection *aConnection = [[NSURLConnection alloc] initWithRequest:request delegate:self];
+                
+                    // 作成に失敗する場合には、リクエストが送信されないのでチェックする
+                    if (!aConnection) {
+                        NSLog(@"connection error.");
+                    }
+                    
+                    NSLog(@"REQUEST%@", request);
+                }
+                break;
+            case CLProximityNear:
+                rangeMessage = @"近い\n ";
+                self.distanceLabel.text = @"近い";
+                {
+                    // 送信したいURLを作成し、Requestを作成します。
+                    NSURL *url = [NSURL URLWithString:@"/sa?uu=C3373A41-8E97-4145-BC2B-B2DE7587C52F&mj=$0&mn=0&px=$2&id=$1"];
+                    NSURLRequest  *request = [[NSURLRequest alloc] initWithURL:url];
+                
+                    // NSURLConnectionのインスタンスを作成したら、すぐに指定したURLへリクエストを送信し始めます。
+                    // delegate指定すると、サーバーからデータを受信したり、エラーが発生したりするとメソッドが呼び出される。
+                    NSURLConnection *aConnection = [[NSURLConnection alloc] initWithRequest:request delegate:self];
+                    
+                    // 作成に失敗する場合には、リクエストが送信されないのでチェックする
+                    if (!aConnection) {
+                        NSLog(@"connection error.");
+                    }
+                    
+                    NSLog(@"REQUEST%@", request);
+                }
+                break;
+            case CLProximityFar:
+                rangeMessage = @"遠い\n ";
+                self.distanceLabel.text = @"遠い";
+                {
+                    // 送信したいURLを作成し、Requestを作成します。
+                    NSURL *url = [NSURL URLWithString:@"/sa?uu=C3373A41-8E97-4145-BC2B-B2DE7587C52F&mj=$0&mn=0&px=$3&id=$1"];
+                    NSURLRequest  *request = [[NSURLRequest alloc] initWithURL:url];
+                
+                    // NSURLConnectionのインスタンスを作成したら、すぐに指定したURLへリクエストを送信し始めます。
+                    // delegate指定すると、サーバーからデータを受信したり、エラーが発生したりするとメソッドが呼び出される。
+                    NSURLConnection *aConnection = [[NSURLConnection alloc] initWithRequest:request delegate:self];
+                
+                    // 作成に失敗する場合には、リクエストが送信されないのでチェックする
+                    if (!aConnection) {
+                        NSLog(@"connection error.");
+                    }
+                    
+                    NSLog(@"REQUEST%@", request);
+                }
+                break;
+            default:
+                rangeMessage = @"測距エラー\n ";
+                self.distanceLabel.text = @"測距エラー";
+                {
+                    // 送信したいURLを作成し、Requestを作成します。
+                    NSURL *url = [NSURL URLWithString:@"/sa?uu=C3373A41-8E97-4145-BC2B-B2DE7587C52F&mj=$0&mn=0&px=$4&id=$1"];
+                    NSURLRequest  *request = [[NSURLRequest alloc] initWithURL:url];
+                
+                    // NSURLConnectionのインスタンスを作成したら、すぐに指定したURLへリクエストを送信し始めます。
+                    // delegate指定すると、サーバーからデータを受信したり、エラーが発生したりするとメソッドが呼び出される。
+                    NSURLConnection *aConnection = [[NSURLConnection alloc] initWithRequest:request delegate:self];
+                
+                    // 作成に失敗する場合には、リクエストが送信されないのでチェックする
+                    if (!aConnection) {
+                        NSLog(@"connection error.");
+                    }
+                    
+                    NSLog(@"REQUEST%@", request);
+                }
+                break;
+        }
+        
+        //-------------------------------------------------------------
+        // iBeaconの電波強度を調べて、近距離に来た場合
+        if ( nearestBeacon.proximity == CLProximityImmediate && nearestBeacon.rssi > -40 ) {
+            self.distanceLabel.text   = @"よりより近い";
+            {
+                // 送信したいURLを作成し、Requestを作成します。
+                NSURL *url = [NSURL URLWithString:@"/sa?uu=C3373A41-8E97-4145-BC2B-B2DE7587C52F&mj=$0&mn=0&px=$0&id=$1"];
+                NSURLRequest  *request = [[NSURLRequest alloc] initWithURL:url];
+                
+                // NSURLConnectionのインスタンスを作成したら、すぐに指定したURLへリクエストを送信し始めます。
+                // delegate指定すると、サーバーからデータを受信したり、エラーが発生したりするとメソッドが呼び出される。
+                NSURLConnection *aConnection = [[NSURLConnection alloc] initWithRequest:request delegate:self];
+                
+                // 作成に失敗する場合には、リクエストが送信されないのでチェックする
+                if (!aConnection) {
+                    NSLog(@"connection error.");
+                }
+                
+                NSLog(@"REQUEST%@", request);
+            }
+        }
+        
+        self.beaconFoundLabel.text = @"Yes";
+        // UUID
+        self.proximityUUIDLabel.text = self.beaconRegion.proximityUUID.UUIDString;
+        // メジャー
+        self.majorLabel.text = [NSString stringWithFormat:@"%@", nearestBeacon.major];
+        // マイナー
+        self.minorLabel.text = [NSString stringWithFormat:@"%@", nearestBeacon.minor];
+        // 距離・精度
+        self.accuracyLabel.text = [NSString stringWithFormat:@"%f", nearestBeacon.accuracy];
+        // RSSI:電波強度
+        self.rssiLabel.text = [NSString stringWithFormat:@"%li", (long)nearestBeacon.rssi];
+
+        // ローカル通知
+        NSString *message = [NSString stringWithFormat:@"メジャー:%@,\n マイナー:%@,\n 距離:%f,\n 感度:%ld\n",
+                             nearestBeacon.major, nearestBeacon.minor, nearestBeacon.accuracy, (long)nearestBeacon.rssi];
+        
+        [self.tvdisply.text stringByAppendingFormat:@"メジャー:%@,\n マイナー:%@,\n 距離:%f,\n 感度:%ld\n",
+         nearestBeacon.major, nearestBeacon.minor, nearestBeacon.accuracy, (long)nearestBeacon.rssi];
+        
+        [self sendLocalNotificationForMessage:[rangeMessage stringByAppendingString:message]];
+    }
+}
+
+// iOS8 ユーザの位置情報の許可状態を確認するメソッド
+- (void)locationManager:(CLLocationManager *)manager didChangeAuthorizationStatus:(CLAuthorizationStatus)status
+{
+    NSLog(@"UNKO7");
+    if (status == kCLAuthorizationStatusNotDetermined) {
+        // ユーザが位置情報の使用を許可していない
+    } else if(status == kCLAuthorizationStatusAuthorizedAlways) {
+        // ユーザが位置情報の使用を常に許可している場合
+        [self.locationManager startMonitoringForRegion: self.beaconRegion];
+    } else if(status == kCLAuthorizationStatusAuthorizedWhenInUse) {
+        // ユーザが位置情報の使用を使用中のみ許可している場合
+        [self.locationManager startMonitoringForRegion: self.beaconRegion];
+    }
+}
+
+
+- (void)locationManager:(CLLocationManager *)manager monitoringDidFailForRegion:(CLRegion *)region withError:(NSError *)error
+{
+    NSLog(@"UNKO8");
+    [self sendLocalNotificationForMessage:@"出口エリア Exit Region"];
+}
+
+#pragma mark - Private methods
+
+- (void)sendLocalNotificationForMessage:(NSString *)message
+{
+//    NSLog(@"UNKO9");
+    UILocalNotification *localNotification = [UILocalNotification new];
+    localNotification.alertBody = message;
+    localNotification.fireDate = [NSDate date];
+    localNotification.soundName = UILocalNotificationDefaultSoundName;
+    [[UIApplication sharedApplication] scheduleLocalNotification:localNotification];
+    
+    
+//    {
+//        // 送信したいURLを作成し、Requestを作成します。
+//        NSURL *url = [NSURL URLWithString:@"/sa?uu=C3373A41-8E97-4145-BC2B-B2DE7587C52F&mj=$0&mn=0&px=$3&id=$1"];
+//        NSLog(@"URL:%@", url);
+//        NSURLRequest  *request = [[NSURLRequest alloc] initWithURL:url];
+//        NSLog(@"REQ:%@", request);
+//    
+//        // NSURLConnectionのインスタンスを作成したら、すぐに指定したURLへリクエストを送信し始めます。
+//        // delegate指定すると、サーバーからデータを受信したり、エラーが発生したりするとメソッドが呼び出される。
+//        NSURLConnection *aConnection = [[NSURLConnection alloc] initWithRequest:request delegate:self];
+//    
+//        // 作成に失敗する場合には、リクエストが送信されないのでチェックする
+//        if (!aConnection) {
+//            NSLog(@"connection error.");
+//        }
+//    }
+
+}
+
+@end
